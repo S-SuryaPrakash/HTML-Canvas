79bd455e6b5be5ec12275a900ea2b7d4397643e5
S.Surya Prakash <s.suryaprakash7767@gmail.com>
2023-08-23 00:07:42 +0530

    added an animation with a detection collision

--- script.js
+++ script.js
@@ -28,35 +28,72 @@ const ctx = canvas.getContext("2d");
 // ctx.moveTo(50,50);
 
 //made a smiley face
-ctx.beginPath();
-// ctx.strokeStyle='white'
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
-//face
-ctx.arc(centerX, centerY, 200, 0, Math.PI * 2, true);
-// mouth
-ctx.moveTo(centerX + 100, centerY);
-ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
-//left eye
-ctx.moveTo(centerX - 60, centerY - 80);
-ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2, false);
-// right eye
-ctx.moveTo(centerX + 100, centerY - 80);
-ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2, false);
-ctx.stroke();
-
+// ctx.beginPath();
+// // ctx.strokeStyle='white'
+// const centerX = canvas.width / 2;
+// const centerY = canvas.height / 2;
+// //face
+// ctx.arc(centerX, centerY, 200, 0, Math.PI * 2, true);
+// // mouth
+// ctx.moveTo(centerX + 100, centerY);
+// ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
+// //left eye
+// ctx.moveTo(centerX - 60, centerY - 80);
+// ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2, false);
+// // right eye
+// ctx.moveTo(centerX + 100, centerY - 80);
+// ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2, false);
+// ctx.stroke();
 
 //Quadratic curves
 // Syntax:-quadraticCurveTO(cp1x,cp1y,x,y) there will be a point for the curve through which we can adjust
 
 //Bezier curve
 // Syntax:-bezierCurveTo(cp1x,cp1y,cp2x,cp2y,x,y) here we will have two points to alter the curve hence it takes two co-ordinates of x and y
-ctx.beginPath();
-ctx.moveTo(75, 25);
-ctx.quadraticCurveTo(25, 25, 25, 62.5);
-ctx.quadraticCurveTo(25, 100, 50, 100);
-ctx.quadraticCurveTo(50, 120, 30, 125);
-ctx.quadraticCurveTo(60, 120, 65, 100);
-ctx.quadraticCurveTo(125, 100, 125, 62.5);
-ctx.quadraticCurveTo(125, 25, 75, 25);
-ctx.stroke();   
+// ctx.beginPath();
+// ctx.moveTo(75, 25);
+// ctx.quadraticCurveTo(25, 25, 25, 62.5);
+// ctx.quadraticCurveTo(25, 100, 50, 100);
+// ctx.quadraticCurveTo(50, 120, 30, 125);
+// ctx.quadraticCurveTo(60, 120, 65, 100);
+// ctx.quadraticCurveTo(125, 100, 125, 62.5);
+// ctx.quadraticCurveTo(125, 25, 75, 25);
+// ctx.stroke();
+
+//Animation
+const circle = {
+  x: 200,
+  y: 200,
+  size: 30, //radius of the circle
+  dx: 5, //dx and dy are the increments that we wouldl like to have in the movement of x and y axis
+  dy: 4,
+};
+
+function drawCircle() {
+  ctx.beginPath();
+  ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2, true);
+  ctx.fillStyle = "purple";
+  ctx.fill();
+}
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawCircle();
+  circle.x += circle.dx;
+  circle.y+=circle.dy;
+//detect collision/side walls
+if(circle.x>canvas.width||circle.x-circle.size<0){
+    circle.dx*=-1;
+}
+
+//detect top and bottom wall
+if(circle.y>canvas.height || circle.y-circle.size<0){
+    circle.dy*=-1;
+}
+
+  requestAnimationFrame(update);
+
+}
+
+// drawCircle();
+update();
